670705ec36f3d59e1d6432f13e905e6603533f66
RupeshKas <kashyaprupesh99@example.com>
2021-08-05 12:37:57 +0530

    API model

---
+++ City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    [Table("City")]
+    public class City
+    {
+        [Key]
+        
+        public int Id { get; set; }
+
+        [StringLength(30)]
+        public string  CityName { get; set; }
+    }
+}

92d418fad16958e3124c548a13666fa1bf46c9ec
Rupesh <kashyaprupesh99@gmail.com>
2021-08-05 13:54:44 +0530

    Add files via upload

---
+++ Property.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    [Table("Property")]
+    public class Property
+    {
+        [Key]
+
+        public int PId { get; set; }
+
+        [StringLength(30)]
+        public string PropertyName { get; set; }
+
+        [StringLength(30)]
+        public string PropertyType { get; set; }
+
+        [StringLength(30)]
+        public string PropertyDcrp { get; set; }
+
+        public double PropertyPrice { get; set; }
+    }
+}
